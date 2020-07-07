4ecc553e5aef522ff53d9dc0704047ba9a3c17ec
AraceliRdz <51061115+AraceliRdz@users.noreply.github.com>
2020-07-06 19:20:43 -0500

    Update Grupos.php

--- App/Controllers/Admin/Grupos.php
+++ App/Controllers/Admin/Grupos.php
@@ -42,7 +42,7 @@ class Grupos extends \Core\Controller
     {      
         try 
         {  
-            $elementos = Grupo::obtener($this->route_params['id']);
+            $obtenidos = Grupo::obtener($this->route_params['id']);
             View::renderTemplate('Grupos/editar.html', [
                 'obtenidos' => $obtenidos
             ]);
